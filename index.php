9655a01bc5401a66db659d32a0d7fb05f94ea775
swimwithfishes <alterhit@gmail.com>
2020-04-02 13:14:50 +0200

    refreshing my brain

---
+++ index.php
@@ -0,0 +1,18 @@
+hello world </br>
+
+<?php
+
+$name = 'Bud';
+$age = 40;
+$location = 'Barcelona' ;
+$job = 'Founder' ;
+
+echo "Hi, my name is <strong>$name</strong> and i am <strong>$age</strong> years old. I live in <strong>$location</strong> and work as the <strong>$job</strong> of my website ";
+
+
+$x = 4;
+$y = 10;
+
+
+echo "</br>" . $x * $y
+?>
